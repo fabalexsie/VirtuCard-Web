06ad10d96b49629a43ff82e7208babeadb4f2d04
Fabian <50114802+fabalexsie@users.noreply.github.com>
2023-10-27 00:43:32 +0200

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { apiRouter } from './api';
+dotenv.config();
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.static('frontend/build'));
+
+app.use('/api', apiRouter);
+
+// nothing else matches, so send index.html (react will handle the rest)
+app.use('*', (req, res) => {
+  res.sendFile('index.html', { root: 'frontend/build' });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
