705d58f54a853c3bb6ee6386388d6dd32775a0a0
Fabian <50114802+fabalexsie@users.noreply.github.com>
2023-11-28 00:57:12 +0100

    Rendering in full height

--- frontend/src/components/EjsRenderer.tsx
+++ frontend/src/components/EjsRenderer.tsx
@@ -88,6 +88,7 @@ export function EjsRenderer({
   if (renderedHtmlStr && !renderError) {
     return (
       <div
+        className="flex flex-col w-full h-full"
         dangerouslySetInnerHTML={{ __html: renderedHtmlStr }}
         ref={outerRef}
       />
