7dc922bcc6a491548373623ac8da1dda2fbb25c5
Fabian <50114802+fabalexsie@users.noreply.github.com>
2024-12-25 18:34:13 +0100

    save data as vcf

--- frontend/src/utils/utilContact.ts
+++ frontend/src/utils/utilContact.ts
@@ -3,18 +3,49 @@ import { downloadAsFile } from './util';
 import vCard from 'vcf';
 
 export function downloadVcf(personData: Person) {
-  // TODO: add more fields to the vCard (vcf)
   let card = new vCard();
-  card.add('fn', personData.firstname);
-  for (const email of [personData.email]) {
-    if (email) card.add('email', email);
-  }
-  for (const phone of [personData.phone]) {
-    if (phone) card.add('tel', phone, { type: 'work' });
-  }
-  // n, org, title, photo, tel, adr
+
+  // Name
+  card.add('n', `${personData.lastname};${personData.firstname};;;`);
+
+  // Fullname
+  card.add('fn', `${personData.firstname} ${personData.lastname}`);
+
+  // Title
+  if (personData.position) card.add('title', personData.position);
+
+  // Tel
+  if (personData.phone) card.add('tel', personData.phone, { type: 'work' });
+
+  // Email
+  if (personData.email) card.add('email', personData.email, { type: 'work' });
+
+  // Address
+  if (personData.address) card.add('adr', personData.address, { type: 'work' });
+
+  // Website
+  if (personData.website)
+    card.add('url', `https://${personData.website}`, { type: 'work' });
+
+  // LinkedIn
+  if (personData.linkedin)
+    card.add('url', `https://linkedin.com/in/${personData.linkedin}`, {
+      type: 'linkedin',
+    });
+  // GitHub
+  if (personData.github)
+    card.add('url', `https://github.com/${personData.github}`, {
+      type: 'github',
+    });
+
+  // Notes
+  if (personData.notes) card.add('note', personData.notes);
+
+  // Photo
+  if (personData.portraitData) card.add('photo', personData.portraitData);
+
   downloadAsFile(
-    `${personData.firstname}.vcf`,
+    `${personData.lastname}_${personData.firstname}.vcf`,
     card.toString(),
     vCard.mimeType,
   );
